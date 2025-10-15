7302b7b4e2c66dba5c5dfa3f270ae70782b7292f
krishnadev4149 <kishangupta4149@gmail.com>
2025-10-15 23:24:05 +0530

    Added power calculator program using pow() in C
    This program takes an integer input 'a' from the user and calculates a^a (a raised to the power of itself) using the pow() function from <math.h>.
    
    ✨ Key Learning Points:
    - Practiced basic input/output using scanf() and printf().
    - Learned to use the <math.h> library and the pow() function.
    - Understood the importance of correct format specifiers ("%d").
    - Differentiated between passing variable values and memory addresses.
    - Explored integer vs. double type handling in mathematical operations.
    
    💡 Example:
    Input: 4
    Output: 256
    
    This is part of my C language practice journey — starting with small programs to understand syntax, logic flow, and basic math operations in C.

---
+++ a^a.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int a;
+    printf("enter a : ");
+    scanf("%d", &a);
+    
+    int result = pow(a,a);
+    printf("%d", result);
+
+    return 0;
+}
